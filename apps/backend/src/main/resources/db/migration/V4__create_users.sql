f805f14fa0f5e642ac9ba2037438408cb30c94b2
SingHo <singho@example.com>
2025-09-21 20:04:49 +0800

    Added back user file

---
+++ apps/backend/src/main/resources/db/migration/V4__create_users.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    about_me VARCHAR(500),
+    role VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    admin BOOLEAN NOT NULL
+);
+INSERT INTO users (name, email, about_me, role, password, admin)
+VALUES (
+    'Admin User',
+    'admin@example.com',
+    'Administrator account',
+    'ADMIN',
+    '$2a$12$0jS8DjLqfkBmxRonGT2vd.gWPYzyxnhbMoebvcWQsOLqGJ6ckqrYy',
+    true
+);
